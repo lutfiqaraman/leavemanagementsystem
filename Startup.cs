bc408384c201d3487b3b291a5d8036bbe290f65f
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-09 14:26:01 +0300

    enbale razor file compilation

--- Startup.cs
+++ Startup.cs
@@ -17,16 +17,23 @@ namespace leavemanagementsystem
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
+        public IWebHostEnvironment Env { get; set; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            IMvcBuilder builder = services.AddRazorPages();
+
+            if (Env.IsDevelopment())
+                builder.AddRazorRuntimeCompilation();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
