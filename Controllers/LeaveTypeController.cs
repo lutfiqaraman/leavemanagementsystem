7708f788f94746ffd249362bccf2abd211051893
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-17 18:16:03 +0300

    create a LeaveType Controller

---
+++ Controllers/LeaveTypeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leavemanagementsystem.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leavemanagementsystem.Controllers
+{
+    public class LeaveTypeController : Controller
+    {
+        public ILeaveTypeRepository Repo { get; private set; }
+        public IMapper Mapper { get; private set; }
+        
+
+        public LeaveTypeController(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            Repo = leaveTypeRepository;
+            Mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
