987d0052d0df374c2565dd18bf44c8ea8712c767
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-19 20:00:24 +0300

    add Description field to  leavetypes

--- Data/Entities/LeaveType.cs
+++ Data/Entities/LeaveType.cs
@@ -12,6 +12,7 @@ namespace leavemanagementsystem.Data.Entities
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime DateCreated { get; set; }
     }
 }
