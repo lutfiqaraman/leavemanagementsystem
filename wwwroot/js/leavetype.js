53a18dc95587bc9e33984fbbc0c29212de5ce767
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-19 20:16:54 +0300

    create a seperate javascript file

---
+++ wwwroot/js/leavetype.js
@@ -0,0 +1,14 @@
+﻿$('document').ready(function () {
+    $('#leavetypetable').DataTable({
+        "ajax": {
+            "url": "/LeaveType/GetLeaveTypes",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "Name" },
+            { "data": "Description" },
+            { "data": "DateCreated" }
+        ]
+    });
+});
