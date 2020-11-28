6cb6bfcf6a4c5358d1e0d295131a30886bae12f6
lutfiqaraman <lutfiqaraman@gmail.com>
2020-11-28 22:23:15 +0200

    do data migration

---
+++ Data/Migrations/20201128201820_AddedNewFields.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leavemanagementsystem.Data.Migrations
+{
+    public partial class AddedNewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultDays",
+                table: "LeaveTypes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Period",
+                table: "LeaveAllocations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "DetailsLeaveTypeViewModel",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultDays",
+                table: "LeaveTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "LeaveAllocations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "DetailsLeaveTypeViewModel");
+        }
+    }
+}
