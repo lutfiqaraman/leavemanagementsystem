7b15d6bf89aaea8a191db8386a883f13c417d61c
lutfiqaraman <lutfiqaraman@gmail.com>
2020-11-30 22:33:14 +0200

    Add data annotation

--- Data/Entities/LeaveType.cs
+++ Data/Entities/LeaveType.cs
@@ -12,7 +12,12 @@ namespace leavemanagementsystem.Data.Entities
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please days should be between 1 and 25")]
+        [Display(Name = "Default Number of Days")]
         public int DefaultDays { get; set; }
+
         public string Description { get; set; }
         public DateTime DateCreated { get; set; }
     }
