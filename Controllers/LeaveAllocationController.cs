2b96cbf3eae66cf8b32ca2a3434a9432ba0f4ab3
lutfiqaraman <lutfiqaraman@gmail.com>
2020-12-11 21:52:34 +0200

    leave allocation controller

--- Controllers/LeaveAllocationController.cs
+++ Controllers/LeaveAllocationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using leavemanagementsystem.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +9,23 @@ using System.Threading.Tasks;
 
 namespace leavemanagementsystem.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class LeaveAllocationController : Controller
     {
+        public ILeaveTypeRepository LeaveTypeRepo { get; private set; }
+        public ILeaveAllocationRepository LeaveAllocationRepo { get; private set; }
+        public IMapper Mapper { get; private set; }
+
+        public LeaveAllocationController(
+            IMapper mapper, 
+            ILeaveTypeRepository leaveTypeRepo, 
+            ILeaveAllocationRepository leaveAllocationRepo)
+        {
+            Mapper = mapper;
+            LeaveTypeRepo = leaveTypeRepo;
+            LeaveAllocationRepo = leaveAllocationRepo;
+        }
+        
         public IActionResult Index()
         {
             return View();
