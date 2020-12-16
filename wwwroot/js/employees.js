8a036c2c5d4dd2d3516b4a772a7f6a895371355f
lutfiqaraman <lutfiqaraman@gmail.com>
2020-12-16 21:29:41 +0200

    write javascript of employees

--- wwwroot/js/employees.js
+++ wwwroot/js/employees.js
@@ -1,1 +1,56 @@
-﻿
+﻿var popUp;
+
+$(document).ready(function () {
+    
+    var table = $("#employeesTable").DataTable({
+        lengthChange: false,
+        select: true,
+        dom: 'Bfrtip',
+
+        ajax: {
+            url: '/employees/GetEmployees',
+            type: 'GET',
+            datatype: 'json'
+        },
+
+        buttons: [
+            {
+                text: 'New',
+                action: function () {
+                    var url = '/employees/AddEditEmployees';
+                    PopupForm(url);
+                }
+            },
+            {
+                text: 'Edit',
+                action: function () {
+                    var id = table.rows({ selected: true }).data()[0].id;
+                    var url = '/employees/AddEditEmployees/' + id;
+
+                    PopupForm(url);
+                }
+            },
+            {
+                text: 'Delete',
+                action: function () {
+                    var id = table.rows({ selected: true }).data()[0].id;
+                    var url = '/employees/DeleteEmployees/' + id;
+
+                    DeleteEmployees(url);
+                }
+            }
+        ],
+
+        columns: [
+            { "data": "username" },
+            { "data": "email" },
+            { "data": "phonenumber" },
+            { "data": "firstname" },
+            { "data": "lastname" },
+            { "data": "taxid" },
+            { "data": "dateofbirth" },
+            { "data": "datejoin" },
+        ]
+    });
+
+});
