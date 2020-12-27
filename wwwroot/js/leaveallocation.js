459b7e966eb2e02c8c33ac3c2eef46add5778d1f
lutfiqaraman <lutfiqaraman@gmail.com>
2020-12-27 21:34:14 +0200

    display leave allocation

---
+++ wwwroot/js/leaveallocation.js
@@ -0,0 +1,51 @@
+﻿var popUp;
+
+$(document).ready(function () {
+
+    var table = $("#leaveAllocationTable").DataTable({
+        lengthChange: false,
+        select: true,
+        dom: 'Bfrtip',
+
+        ajax: {
+            url: '/leavetype/GetLeaveType',
+            type: 'GET',
+            datatype: 'json'
+        },
+
+        buttons: [
+            {
+                text: 'New',
+                action: function () {
+                    var url = '/leavetype/AddEditLeaveType';
+                    PopupForm(url);
+                }
+            },
+            {
+                text: 'Edit',
+                action: function () {
+                    var id = table.rows({ selected: true }).data()[0].id;
+                    var url = '/leavetype/AddEditLeaveType/' + id;
+
+                    PopupForm(url);
+                }
+            },
+            {
+                text: 'Delete',
+                action: function () {
+                    var id = table.rows({ selected: true }).data()[0].id;
+                    var url = '/leavetype/DeleteLeaveType/' + id;
+
+                    DeleteLeaveType(url);
+                }
+            }
+        ],
+
+        columns: [
+            { "data": "name" },
+            { "data": "description" },
+            { "data": "defaultDays" }
+        ]
+    });
+
+});
